268448d221d558c9e7bfbba50f97a28a6e9b38c5
Gian Carlo S. Victorino <155649616+giancarlo0326@users.noreply.github.com>
2024-06-12 22:41:30 +0800

    Add files via upload
    nothing much, just added some pages to make it complete (a little bit)

---
+++ viewrentals.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View your Rentals - PUIHAHA Videos</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <nav>
+        <a class="home-link" href="index.php">
+            <img src="https://i.postimg.cc/CxLnK8q1/PUIHAHA-VIDEOS.png" alt="Home">
+        </a>
+        <input type="checkbox" id="sidebar-active">
+        <label for="sidebar-active" class="open-sidebar-button">
+            <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+        </label>
+        <label id="overlay" for="sidebar-active"></label>
+        <div class="links-container">
+            <label for="sidebar-active" class="close-sidebar-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+            </label>
+            <a href="dvd.php">DVDs</a> 
+            <a href="bluray.php">Blu-Rays</a>
+            <a href="digitals.php">Digitals</a>
+            <a href="viewallcatalogs.php">View All Catalogs</a>
+            <a href="viewrentals.php">View Rentals</a>
+            <a href="aboutdevs.php">About the Devs</a>
+            <a href="login.php">Login</a>
+        </div>
+    </nav>
+
+    
+
+<div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="centered-container">
+                    <div class="container">
+                        <h1>Rent <span class="auto-type"></span> </h1>
+                    </div>
+                    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+                    <script>
+                        var typed = new Typed(".auto-type",{
+                        strings : ["DVDs", "Blu-Rays", "Digitals", "with PUIHAHA Videos."],
+                        typeSpeed : 100,
+                        backSpeed : 100,
+                        looped : true
+                        })
+                    </script>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
